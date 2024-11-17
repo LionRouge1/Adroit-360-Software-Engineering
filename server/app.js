f4f8567b08eb53994b64d838ea707ab32345dc58
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-17 23:06:30 +0000

    Create route to save create new agent

--- server/app.js
+++ server/app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const { Agent } = require('./model');
 
 const app = express();
+app.use(express.json());
 
 app.get('/agents', async (req, res, next) => {
   const agents = await Agent.findAll();
+  return res.json(agents);
+});
+
+app.post('/agents', async (req, res) => {
+  console.log('Request body:', req.body);
+  const agents = await Agent.create(req.body.agentData);
   return res.json(agents);
 });
 
