53e099a53644f68d85962b5e38f0837145fe15c3
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-18 11:23:41 +0000

    Create component to handle messages

---
+++ src/components/message/MessageDisplay.tsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { MessageContext } from '../../contexts/MessageContext';
+
+const MessageDisplay: React.FC = () => {
+  const { message, clearMessage } = useContext(MessageContext);
+
+  if(!message.type) return null;
+  return (
+    <div className={`alert alert-${message.type}`} role="alert">
+      <span>{message.message}</span>
+      <button type="button" onClick={clearMessage} className="btn">Dismiss</button>
+    </div>
+  );
+};
+
+export default MessageDisplay;
