abacbdfda8c7184901a64a9499a7626af45715f7
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-18 01:56:35 +0000

    Change agent style

--- src/components/Agents/Agent.tsx
+++ src/components/Agents/Agent.tsx
@@ -1,28 +1,21 @@
 import type { FC } from "react";
+import { Link } from "react-router-dom";
 import { IAgent } from "../../types/Agent";
+import { truncateText } from "../../hooks/useAgent";
 
 import './Agent.css'
 
+
 const Agent: FC<{ agent: IAgent }> = ({ agent }) => {
+  const { id, firstName, lastName, photoUrl, aboutMe } = agent;
   return (
-    <div className="container">
-      <header>
-        <div className="avatar-holder">
-          <img src={agent.photoUrl} className="avatar" alt={agent.firstName} />
-        </div>
-        <h2 className="agent-name">{agent.firstName + " " + agent.lastName}</h2>
-      </header>
-      <div className="body">{agent.aboutMe}</div>
-      <footer>
-        <div className="full-width-flex-box">
-          <div className="one-third-flex-box">
-            <span>{agent.address}</span>
-          </div>
-          <div className="one-third-flex-box">
-            <span>Areas of Practice: {agent.practiceAreas}</span>
-          </div>
-        </div>
-      </footer>
+    <div className="card" style={{ width: '18rem' }}>
+      <img className="card-img-top" src={photoUrl} alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{`${firstName} ${lastName}`}</h5>
+        <p className="card-text">{truncateText(aboutMe, 30)}</p>
+        <Link to={`/agents/${id}`} className="btn btn-primary">Read more...</Link>
+      </div>
     </div>
   );
 };
