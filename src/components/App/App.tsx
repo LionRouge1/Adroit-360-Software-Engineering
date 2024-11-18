593db47357b24e1bb992079868d149e81e39e2c1
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-18 01:59:05 +0000

    Create a routes for the views

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,17 @@ import type { FC } from "react";
 import "./App.css";
 
 import Agents from "../Agents/Agents";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AgentDetails from "../Agents/AgentDetails";
 
 const App: FC = () => {
   return (
-    <div className="app">
-      <Agents />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Agents />} />
+        <Route path="agents/:id" element={<AgentDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
