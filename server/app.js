bbd7ba27824a81648f65824f5d10642dc070e53e
LionRouge1 <matchoudiavlessi@gmail.com>
2024-11-18 01:52:24 +0000

    Create route to find agent by pk

--- server/app.js
+++ server/app.js
@@ -15,4 +15,14 @@ app.post('/agents', async (req, res) => {
   return res.json(agents);
 });
 
+app.get('/agents/:id', async (req, res) => {
+  console.log('Request body:', req.params.id);
+  const agent = await Agent.findByPk(req.params.id);
+
+  if (!agent) {
+    return res.status(404).end();
+  }
+  return res.json(agent);
+});
+
 module.exports = app;
